813359e66c9494c9ddf7a70cebac0560165a3e4a
Win <131394482+win-ne@users.noreply.github.com>
2024-11-19 21:58:38 +0300

    chore: adjust site title name

--- apps/dma-frontend/app/page.tsx
+++ apps/dma-frontend/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
   return (
     <div className="flex items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-6 justify-center">
-        <Header title="Digital Marketing Studio" />
+        <Header title="Digital Marketing Agency" />
         <p className="text-3xl ms-4  font-extrabold">Our Services</p>
         <div className="w-full flex flex-col gap-4">
           {categories.map((cat) => <CategoryCard linkable={true} category={cat} key={`category-${cat.documentId}`} />)}
